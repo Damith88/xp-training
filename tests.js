e7939322e6222640115335dff7fda294ab51f95a
Pathumhdes <pathumhdes@gmail.com>
2015-08-11 18:34:14 +0530

    Commiting logic

--- tests.js
+++ tests.js
@@ -3,9 +3,13 @@ QUnit.test( "hello test", function( assert ) {
 });
 
 QUnit.test("Testing Plus function", function(assert) {
-  assert.equal(5, plus(2, 3), "3 + 2 is 5");
+  assert.equal(5, calculate(2, 3, "+"), "3 + 2 is 5");
+});
+
+QUnit.test("Testing Minus function with failing", function(assert) {
+  assert.equal(2, calculate(4, 2, "-"), "4 - 2 is 2");
 });
 
 QUnit.test("Testing Plus function with failing", function(assert) {
-  assert.equal(5, plus(2, 2), "2 + 2 is 5");
+  assert.equal(0, calculate(2, 2, "-"), "2 + 2 is 4");
 });
